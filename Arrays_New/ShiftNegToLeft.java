0363f4604619dcb0184aac7c0f31e1f8e7a40262
Sanket Shingadiya <sit457@sit.local>
2025-06-23 11:08:29 +0530

    Shift all neg to left

--- Arrays_New/ShiftNegToLeft.java
+++ Arrays_New/ShiftNegToLeft.java
@@ -2,6 +2,33 @@ package Arrays_New;
 
 public class ShiftNegToLeft {
     public static void main(String[] args) {
-        Integer.max(0, 0)
+        int[] arr = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+
+            // increment left while arr[left]
+            // is negative
+            while (left < right && arr[left] < 0) {
+                left++;
+            }
+
+            // decrement right while arr[right]
+            // is positive
+            while (right > left && arr[right] > 0) {
+                right--;
+            }
+
+            // swap the two values.
+            if (right > left) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
     }
+}
 }
